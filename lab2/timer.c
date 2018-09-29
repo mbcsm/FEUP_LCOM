db3ec5229735be1294b16e7ee0566d64a152f4b6
mbcsm <manelbcsm1@gmail.com>
2018-09-29 21:31:10 +0100

    timer_test_read_config DONE

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,43 +5,86 @@
 
 #include "i8254.h"
 
-int (timer_set_frequency)(uint8_t UNUSED(timer), uint32_t UNUSED(freq)) {
+int(timer_set_frequency)(uint8_t UNUSED(timer), uint32_t UNUSED(freq)) {
   /* To be completed by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
 }
 
-int (timer_subscribe_int)(uint8_t *UNUSED(bit_no)) {
+int(timer_subscribe_int)(uint8_t *UNUSED(bit_no)) {
   /* To be completed by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
 }
 
-int (timer_unsubscribe_int)() {
+int(timer_unsubscribe_int)() {
   /* To be completed by the students */
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
 }
 
-void (timer_int_handler)() {
+void(timer_int_handler)() {
   /* To be completed by the students */
   printf("%s is not yet implemented!\n", __func__);
 }
 
-int (timer_get_conf)(uint8_t UNUSED(timer), uint8_t *UNUSED(st)) {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+/* creator: Manuel Monteiro*/
+int(timer_get_conf)(uint8_t timer, uint8_t *st) {
 
-  return 1;
+  uint8_t byte = TIMER_RB_CMD |
+                 TIMER_RB_COUNT_ |
+                 TIMER_RB_SEL(timer);
+
+  uint32_t st32Temp;
+
+
+
+  if (sys_outb(TIMER_CTRL, byte) != OK) {
+    printf("timer config = Error");
+    return 1;
+  }
+  if (sys_inb(TIMER_CTRL, &st32Temp) != OK) {
+    printf("timer config = Error");
+    return 1;
+  }
+  
+  *st = (uint8_t)st32Temp;
+  printf("Timer Get Conf: DONE\n");
+
+  return 0;
 }
 
-int (timer_display_conf)(uint8_t UNUSED(timer), uint8_t UNUSED(st),
-                        enum timer_status_field UNUSED(field)) {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+/* creator: Manuel Monteiro*/
+int(timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field UNUSED(field)) {
+  printf("\n\n\n_______TIMER CONFIG_________\n", st);
+  printf("Status Byte: %x\n", st);
+  printf("Selected Counter: %d\n", timer);
+  
+
+  if(TIMER_LSB_MSB & st)
+    printf("Type of Access: LSB followed by MSB\n");
+  else if(TIMER_MSB & st)
+    printf("Type of Access: MSB\n");
+  else if(TIMER_LSB & st)
+    printf("Type of Access: LSB\n");
+                      
+  if(TIMER_SQR_WAVE & st)
+    printf("Operating Mode: Mode 3 (square wave generator)\n");
+  else if(TIMER_RATE_GEN & st)
+    printf("Operating Mode: Mode 2 (rate generator)\n");
+  else
+    printf("Operating Mode: ERROR 404 MODE NOT FOUND\n");
+
+    if(TIMER_BCD & st)
+      printf("Counting Mode: BCD\n");
+    else if(TIMER_BIN & st)
+      printf("Counting Mode: Binary\n");
+    else
+      printf("Counting Mode: ERROR 404 MODE NOT FOUND\n");
+
 
   return 1;
 }
