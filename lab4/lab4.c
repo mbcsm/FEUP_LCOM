c239da5472959281f1f79abca80edcf1a408e41d
mbcsm <manelbcsm1@gmail.com>
2018-11-09 10:58:06 +0000

    gesture working without tolerance

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -49,7 +49,7 @@ int (mouse_test_packet)(uint32_t cnt) {
 	int r;
 
     mouse_enable_data_reporting();
-    //enable_data_report();
+    enable_data_report();
 
 	mouse_subscribe_int(&bit_no);
 
@@ -132,7 +132,7 @@ int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
     return 0;
 }
 
-//extern int counter;
+extern int counter;
 int (mouse_test_async)(uint8_t idle_time) {
     /* To be completed */
     printf("%s(%u): under construction\n", __func__, idle_time);
@@ -145,8 +145,7 @@ int (mouse_test_async)(uint8_t idle_time) {
 	int ipc_status;
 	message msg;
 	uint8_t bit_no;
-	int r;
-
+	int r ;
 	if(timer_subscribe_int(&bit_no) != OK)
 		return -1;
 	
@@ -202,6 +201,106 @@ int (mouse_test_gesture)(uint8_t x, uint8_t t) {
     /* To be completed */
     x = t;
     printf("%s: under construction\n", __func__);
-    return 1;
+    
+    extern uint32_t mouseData;
+    uint32_t byteCounter = 0;
+    struct packet pp;
+
+	int ipc_status;
+	message msg;
+	uint8_t bit_no;
+	int r, stop = 0;
+
+    mouse_enable_data_reporting();
+    //enable_data_report();
+
+	mouse_subscribe_int(&bit_no);
+
+	uint64_t irq_set = BIT(bit_no);
+
+    int state = 0, length_of_line = 0;
+
+	while (stop == 0) {
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		//printf("successful\n");
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE: /* hardware interrupt notification */
+					if (msg.m_notify.interrupts & irq_set) {
+						mouseData = 0;   
+
+                        mouse_ih();                        
+                        byteCounter++;
+						
+                        parseToPacket(byteCounter % 3, mouseData, &pp);
+
+                        if (byteCounter % 3 == 0){
+                            switch(state){
+                                case 0:
+                                    if(pp.rb == 1 && pp.lb == 0){
+                                        printf("rb is down -> start drawing the inverted V\n");
+                                        state = 1;
+                                    }
+                                    break;
+                                case 1:
+                                    if(pp.rb == 1 && pp.delta_y > 0 && pp.delta_x > 0){
+                                        length_of_line += pp.delta_x * pp.delta_x + pp.delta_y * pp.delta_y;
+                                        printf("current line lenght: %d\n", length_of_line);
+                                    }else if(pp.rb == 1 && (pp.delta_y <= 0 || pp.delta_x <= 0)){
+                                        printf("FAILED: Wrong Direction\n");
+                                        stop = 1;
+                                    }else if(pp.rb == 0 && length_of_line < x){
+                                        printf("FAILED: line not long enough\n");
+                                        stop = 1;
+                                    }else if(pp.rb == 0 && length_of_line >= x){
+                                        printf("rb-release: line long enough. Press lb to coninue\n");
+                                        length_of_line = 0;
+                                        state = 2;
+                                    }
+                                    break;
+                                case 2:
+                                    if(pp.rb == 0 && pp.lb == 1){
+                                        printf("lb is down -> start drawing the inverted V\n");
+                                        state = 3;
+                                    }else if(pp.rb == 1){
+                                        printf("FAILED: rb pressed. should have been the lb\n");
+                                        stop = 1;
+                                    }
+                                    break;
+                                case 3:
+                                    if(pp.lb == 1 && pp.delta_y < 0 && pp.delta_x > 0){
+                                        length_of_line += pp.delta_x * pp.delta_x + pp.delta_y * pp.delta_y;
+                                        printf("current line lenght: %d\n", length_of_line);
+                                    }else if(pp.lb == 1 && (pp.delta_y >= 0 || pp.delta_x <= 0)){
+                                        printf("FAILED: Wrong Direction\n");
+                                        stop = 1;
+                                    }else if(pp.lb == 0 && length_of_line < x){
+                                        printf("FAILED: line not long enough\n");
+                                        stop = 1;
+                                    }else if(pp.lb == 0 && length_of_line >= x){
+                                        printf("lb-release: line long enough.\nSUCCESS INVERTED V COMPLETED!\n");
+                                        stop = 1;
+                                    }
+                                    break;
+
+                            }
+						    //mouse_print_packet(&pp);
+                            resetPacket(&pp);
+                        }
+					}
+					break;
+				default:
+					break; /* no other notifications expected: do nothing */
+			}
+		}
+	}
+    //disable_data_report();
+	mouse_unsubscribe_int();
+    printf("unsubscribed\n");
+
+    return 0;
 }
 
